198c07cd64786a01937ddda3e9e12f26dca87953
marcus344 <mv67494@gmail.com>
2025-07-21 17:40:11 -0300

    Add files via upload

---
+++ comunidadeimpressionadora/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import  Bcrypt
+from flask_login import LoginManager
+import os
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'e6ead7e1e20e9970d09201cb48dd9943e'
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'alert-info'
+
+
+from comunidadeimpressionadora import routes
